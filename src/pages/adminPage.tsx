d97eb36cd8bd3e07dc8c3864bb62de8c55138a82
JOCECODE <ulah57@gmail.com>
2022-08-14 05:36:10 -0700

    create admin page layout;

--- src/pages/adminPage.tsx
+++ src/pages/adminPage.tsx
@@ -1,7 +1,23 @@
 export default function adminPage() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div id="adminParent" className=" h-[100vh] w-[100wh] bg-red-600">
+          {/* Header */}
+          <div className="flex flex-row">
+            <div className="flex flex-col w-[50%] bg-yellow-500">Logo</div>
+            <div className="flex flex-col w-[50%] bg-purple-500 text-right">Hi, Alyssa Queen</div>
+
+          </div>
+
+          {/* Can add a navbar here when thinking about navigating the website */}
+
+          {/* Content Setup */}
+          <div className="flex flex-row">
+            <div className="flex flex-col w-[25%] h-[96vh] bg-orange-500">left Side Div</div>
+            <div className="flex flex-col w-[75%] h-[96vh] bg-blue-500">right side</div>
+
+          </div>
+      </div>
+    </>
   )
 }
