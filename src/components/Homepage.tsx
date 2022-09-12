38e5b8c82959f09bb78d1a0838006f57fbdaf36e
JOCECODE <ulah57@gmail.com>
2022-09-12 16:52:15 -0700

    add react-calendar component too project;

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -22,18 +22,18 @@ export default function Homepage(){
                             <div id="col">
                             <div id="circle">
                                 
-                        <Image id="icons" width={80} height={80}src={Scissors}/>
+                        <Image id="icons" alt="scissors png" width={80} height={80}src={Scissors}/>
                         <br/>
                         <h4 id="hairService">Hairstyles</h4>
-                        <p id="description">"providing quality cuts to how you want"</p>
+                        <p id="description">${`"providing quality cuts to how you want"`}</p>
                             </div>
                         </div>
                         <div id="col">
                             <div id="circle">
-                        <Image id="icons" width={80} height={80}src={Scissors}/>
+                        <Image id="icons" alt="scissors png" width={80} height={80}src={Scissors}/>
                         <br/>
                         <h4 id="hairService">Haircuts</h4>
-                        <p id="description">"providing quality cuts to how you want"</p>
+                        <p id="description">${`"providing quality cuts to how you want"`}</p>
                             </div>
                     </div>
                     </div>
@@ -47,40 +47,23 @@ export default function Homepage(){
                         <div id="col">
                             <div id="circle">
                                 
-                        <Image id="icons" width={80} height={80}src={Scissors}/>
+                        <Image id="icons" alt="scissors png" width={80} height={80}src={Scissors}/>
                         <br/>
                         <h4 id="hairService">Hair Color</h4>
-                        <p id="description">"providing quality cuts to how you want"</p>
+                        <p id="description">${`"providing quality cuts to how you want"`}</p>
                             </div>
                     </div>
                     </div>
                     </div>
             </div>
 
-          {/* card container is right here on smaller devices it should stay on the right side */}
-        {/* <div id="cardContainer" className="">
-            <div id="card">
-                <h4 className="bookAppointment"><b>Book An Appointment Today!</b></h4> 
-                <p className="whenYouCome">When do you want to come?</p>
-                <p className="select">Select a day</p>
-                <input className=" border-black border-2 border-solid"/>
-                <p className="select">Select a time</p>
-                <input className=" border-black border-2 border-solid"/>
-                <br/>
-                <br/>
-                <button id="bookNow" type="submit">Book Now</button>
-                <br/>
-                <br/>
-                <p className="notice"><b>* Appointments require a 50% deposit to hold the appointment time slot. For full refunds, you MUST cancel before 48 hours from your appointment date.</b></p>
-            </div>
-        </div> */}
-        <div className="flex flex-col justify-center items-center w-[30%] gap-4
+        <div id="cardContainer" className="flex flex-col justify-center items-center w-[30%] gap-4
                         bg-slate-50 text-center rounded-lg 
                         absolute right-[30px] bottom-[-352px]
                         p-7 shadow-lg"
         >
-            <h4 className="font-bold text-3xl m-1">Book an Appointment Today!</h4>
-            <p className="m-1">What works best for you?</p>
+            <h4 className="font-bold text-3xl m-1 bookAppointment">Book an Appointment Today!</h4>
+            <p className="m-1 whenYouCome">What works best for you?</p>
             <Calendar 
                 value={selectedDay}
                 onChange={setSelectedDay}
@@ -88,7 +71,11 @@ export default function Homepage(){
                 shouldHighlightWeekends
             />
             {/* print out selected day to see that its working */}
-            <p>{selectedDay?.month}/{selectedDay?.day}/{selectedDay?.year}</p>
+            {
+                selectedDay ? ( <p>{selectedDay?.month}/{selectedDay?.day}/{selectedDay?.year}</p> ) : "" 
+            }
+
+            <p className="notice"><b>* Appointments require a 50% deposit to hold the appointment time slot. For full refunds, you MUST cancel before 48 hours from your appointment date.</b></p>
         </div>
 
 {/* Main Closing Div */}
