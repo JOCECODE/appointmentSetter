53a7693bec1d3cee6fbcd53a251db0271c1c9917
JOCECODE <ulah57@gmail.com>
2022-09-21 19:52:02 -0700

    incorporate schema sample model;

---
+++ src/config/config.tsx
@@ -0,0 +1,11 @@
+export const config = {
+    firebaseConfig: {
+    apiKey: "AIzaSyAH5OEdtYrP83wo88FBatAoI5vOZvavKog",
+    authDomain: "alyssabeautyparlor.firebaseapp.com",
+    projectId: "alyssabeautyparlor",
+    storageBucket: "alyssabeautyparlor.appspot.com",
+    messagingSenderId: "432801151974",
+    appId: "1:432801151974:web:e9298127bfbfc71d827427",
+    measurementId: "G-P6XF1XH5HZ"
+}
+}
